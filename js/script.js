8aebabc0004faf6e5e161d30c003a35a6abc2a08
Dan Shields <danwshields@gmail.com>
2018-03-28 23:41:05 -0600

    added accordian aminated section open/close.note: section padding must = 0 for max-height method in js to work. AND must use defer tag in js load for code to run AFTER DOM is loaded.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-function toggleclass(id, idclass) {
-  var x = document.getElementById(id);
-  x.classList.toggle(idclass);
-};
+
+var acc = document.getElementsByClassName("accordion");
+
+console.log(acc.length);
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    console.log(panel);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+  console.log('in');
+}
+  console.log('after');
